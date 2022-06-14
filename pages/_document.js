14f2deeb889440515c7df8dd19d52a0209f12719
Daniel Rivera <dr734659@gmail.com>
2022-06-14 10:19:52 -0500

    [ADD] shortcut icon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   
     return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/184/184517.png" type="image/png"/>
+      </Head>
       <body className='my-body-class'>
         <Main />
         <NextScript />
